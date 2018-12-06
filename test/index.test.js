133ffcd409dada423154efb57e50be974c216039
Artsiom Halubotski <timon.test@gmail.com>
2018-12-06 16:37:11 +0300

    Added test framework and few tests

---
+++ test/index.test.js
@@ -0,0 +1,40 @@
+const { expect } = require('chai');
+const { processProductUpdate } = require('../index');
+
+describe('DW product function should', function() {
+	describe('return an error on', function(){
+		it('empty event', function(){
+			expect(processProductUpdate(null)).to.be.an.instanceof(Error).and.to.have.property('message', 'Event data is not valid. Either "data" buffer or "attributes" hash must be specified.');
+		});
+		it('both empty data and attributes properties', function(){
+			expect(processProductUpdate({})).to.be.an.instanceof(Error).and.to.have.property('message', 'Event data is not valid. Either "data" buffer or "attributes" hash must be specified.');
+		});
+		it('data property contains invalid JSON', function(){
+			const data = Buffer.from("this is a test").toString('base64').slice(2);
+			expect(processProductUpdate({ data })).to.be.an.instanceof(SyntaxError);
+		});
+	});
+	describe('return correct object', function(){
+		it('from parsed data property', function(){
+			const source = {
+				test: '2',
+				test2: null,
+				test3: undefined,
+				test4: 120,
+			};
+			const data = Buffer.from(JSON.stringify(source)).toString('base64');
+			// Keys with undefined values do not persist stringify
+			delete source.test3;
+			expect(processProductUpdate({ data })).to.deep.include(source).and.to.not.have.keys('test3');
+		});
+		it('from attribute property', function(){
+			const attributes = {
+				test: '2',
+				test2: null,
+				test3: undefined,
+				test4: 120,
+			};
+			expect(processProductUpdate({ attributes })).to.deep.equal(attributes);
+		});
+	});
+});
