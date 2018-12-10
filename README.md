da22d2138695a1145cffcf0232f2abc305fbffad
Artsiom Halubotski <timon.test@gmail.com>
2018-12-10 13:38:17 +0300

    Added CT SDKs for processing products

---
+++ README.md
@@ -0,0 +1,17 @@
+# Setup
+
+Project depends on Node.js v8
+
+## Installation
+1. Clone repository
+2. Run
+```
+$ npm i
+```
+3. Install mocha global
+```
+$ npm i -g mocha
+```
+
+## Environment
+Set CT access details environment variables: *CT_PROJECT_KEY*, *CT_CLIENT_ID*, *CT_CLIENT_SECRET*, *CT_SCOPES*
--- index.js
+++ index.js
@@ -1,24 +1,75 @@
+const { createClient } = require('@commercetools/sdk-client');
+const { createAuthMiddlewareForClientCredentialsFlow } = require('@commercetools/sdk-middleware-auth');
+const { createHttpMiddleware } = require('@commercetools/sdk-middleware-http');
+const fetch = require('node-fetch');
+
+const projectKey = process.env.CT_PROJECT_KEY;
+const clientId = process.env.CT_CLIENT_ID;
+const clientSecret = process.env.CT_CLIENT_SECRET;
+const scopes = [process.env.CT_SCOPES];
+
+// Setup CT middlewares
+const authMiddleware = createAuthMiddlewareForClientCredentialsFlow({
+	host: 'https://auth.commercetools.co',
+	projectKey,
+	credentials: {
+		clientId,
+		clientSecret,
+	},
+	scopes,
+	fetch,
+});
+const httpMiddleware = createHttpMiddleware({
+	host: 'https://api.commercetools.co',
+	fetch,
+});
+// Setup CT client
+const client = createClient({
+	middlewares: [authMiddleware, httpMiddleware],
+});
+
 /**
- * Triggered from a message on a Cloud Pub/Sub topic.
+ *	Function prepares product from PubSub message
+ *	@param	{Object}	event	MessagePayload
+ *	@returns	{ProductDraft}
+ *	@private
+ */
+function _prepareProductDraft(event) {
+	if(!event || !event.data) {
+		throw new Error('Event object is not valid. "data" buffer must be specified.');
+	}
+	let messageJson = {};
+	try {
+		const pubsubMessage = Buffer.from(event.data, 'base64').toString();
+		messageJson = JSON.parse(pubsubMessage);
+	} catch(err) {
+		throw new SyntaxError('Data property contains invalid json');
+	}
+	return messageJson;
+};
+
+/**
+ * Cloud Pub/Sub message processor to update CT product with DW payload.
  *
  * @param {!Object} event Event payload.
  * @param {!Object} context Metadata for the event.
  */
-exports.processProductUpdate = (event, context) => {
-	if(!event || (!event.data && !event.attributes)) {
-		throw new Error('Event data is not valid. Either "data" buffer or "attributes" hash must be specified.');
-	}
-	if(event.data && event.data.length) {
-		let messageJson = {};
-		try {
-			const pubsubMessage = Buffer.from(event.data, 'base64').toString();
-			messageJson = JSON.parse(pubsubMessage);
-		} catch(err) {
-			throw new SyntaxError('Data property contains invalid json');
-		}
-		return messageJson;
-	} else if (event.attributes) {
-		return event.attributes;
-	}
+function processProductUpdate(event, context) {
+
+	const productDraft = _prepareProductDraft(event);
+
+	const productsRequest = {
+		uri: `/${projectKey}/products`,
+		method: 'POST',
+		body: JSON.stringify(),
+	};
+	client.execute(productsRequest)
+		.then(response => console.dir(response))
+		.catch(error => console.dir(error.body.errors));
+
 };
 
+exports.processProductUpdate = processProductUpdate;
+exports._api = {
+	_prepareProductDraft,
+}
