a0b13a9f42823dc00d9fe119f99c53c35760b886
Artsiom Halubotski <timon.test@gmail.com>
2018-12-19 13:25:13 +0300

    Client initialization moved to a library

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-const { createClient } = require('@commercetools/sdk-client');
-const { createAuthMiddlewareForClientCredentialsFlow } = require('@commercetools/sdk-middleware-auth');
-const { createHttpMiddleware } = require('@commercetools/sdk-middleware-http');
-const fetch = require('node-fetch');
+const { client } = require('./lib/client');
 
 const projectKey = process.env.CT_PROJECT_KEY;
-const clientId = process.env.CT_CLIENT_ID;
-const clientSecret = process.env.CT_CLIENT_SECRET;
-const scopes = [process.env.CT_SCOPES];
 
 const EVENT_MAX_AGE = 20000;
-
-// Setup CT middlewares
-const authMiddleware = createAuthMiddlewareForClientCredentialsFlow({
-	host: 'https://auth.commercetools.co',
-	projectKey,
-	credentials: {
-		clientId,
-		clientSecret,
-	},
-	scopes,
-	fetch,
-});
-const httpMiddleware = createHttpMiddleware({
-	host: 'https://api.commercetools.co',
-	fetch,
-});
-// Setup CT client
-const client = createClient({
-	middlewares: [authMiddleware, httpMiddleware],
-});
 
 /**
  *	Function prepares product from PubSub message
